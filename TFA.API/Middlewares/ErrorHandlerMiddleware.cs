b4d97a1f8a0f4e187487db8830ab6c3d29a96202
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-28 00:56:08 +0200

    add problem detail

--- TFA.API/Middlewares/ErrorHandlerMiddleware.cs
+++ TFA.API/Middlewares/ErrorHandlerMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
-using System.Net;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using TFA.Domain.Authorization;
 using TFA.Domain.Exceptions;
 
@@ -7,7 +8,7 @@ namespace TFA.API.Middlewares
 {
     public class ErrorHandlerMiddleware(RequestDelegate next)
     {
-        public async Task InvokeAsync(HttpContext context)
+        public async Task InvokeAsync(HttpContext context, ProblemDetailsFactory problemDetailsFactory)
         {
             try
             {
@@ -17,17 +18,28 @@ namespace TFA.API.Middlewares
             {
                 var httpCode = e switch
                 {
-                    IntentionManagerException =>StatusCodes.Status403Forbidden,
+                    IntentionManagerException => StatusCodes.Status403Forbidden,
                     ValidationException => StatusCodes.Status400BadRequest,
-                    DomainException domainException=> domainException.ErrorCode switch
+                    DomainException domainException => domainException.ErrorCode switch
                     {
-                        ErrorCode.Gone =>StatusCodes.Status410Gone,
+                        ErrorCode.Gone => StatusCodes.Status410Gone,
                         _ => StatusCodes.Status500InternalServerError
                     },
                     _ => StatusCodes.Status500InternalServerError
                 };
+                var problemDetails = e switch
+                {
+                    IntentionManagerException => problemDetailsFactory.CreateProblemDetails(context, httpCode,
+                    "Authorization failed", detail: e.Message),
+                    ValidationException => problemDetailsFactory.CreateValidationProblemDetails(context, new ModelStateDictionary(),
+                    httpCode, "Invalid request"),
+                    DomainException domainException => problemDetailsFactory.CreateProblemDetails(context, httpCode,
+                    domainException.Message),
+                    _ => problemDetailsFactory.CreateProblemDetails(context, httpCode, "Unhandled error! Please contact us.",
+                    detail: e.Message)
+                };
                 context.Response.StatusCode = httpCode;
-
+                await context.Response.WriteAsJsonAsync(problemDetails);
             }
         }
     }
