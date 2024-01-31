4bd88e48e45e805f21755a8eee73aa135a3c81c1
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-31 14:59:38 +0200

    add integrationTest

--- TFA.Storage/ForumDbContext.cs
+++ TFA.Storage/ForumDbContext.cs
@@ -1,22 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TFA.Storage.Entities;
 
-namespace TFA.Storage
+namespace TFA.Storage;
+
+public class ForumDbContext : DbContext
 {
-    public class ForumDbContext : DbContext
+    public ForumDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
     {
-        public ForumDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
-        {
-            
-        }
-        public DbSet<User> Users { get; set; }
-        public DbSet<Forum> Forums { get; set; }
-        public DbSet<Topic> Topics { get; set; }
-        public DbSet<Comment> Comments { get; set; }
+
     }
+    public DbSet<User> Users { get; set; }
+    public DbSet<Forum> Forums { get; set; }
+    public DbSet<Topic> Topics { get; set; }
+    public DbSet<Comment> Comments { get; set; }
 }
