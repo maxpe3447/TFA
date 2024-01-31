df397eb7a3e285ae0f5130593987d432e8855f92
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-01-31 23:51:41 +0200

    add Integration tests

--- TFA.E2E/ForumEndpointShould.cs
+++ TFA.E2E/ForumEndpointShould.cs
@@ -2,6 +2,8 @@ using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net.Http.Json;
+using TFA.API.Models;
 
 namespace TFA.E2E;
 
@@ -14,15 +16,48 @@ public class ForumEndpointShould:IClassFixture<ForumApiApplicationFactory>
         this.factory = factory;
     }
     [Fact]
-    public async Task ReturnListOfForums()
+    public async Task CreateNewForum()
     {
         using var httpClient = factory.CreateClient();
-        using var response = await httpClient.GetAsync("forums");
+
+        using var getInitialForumsResponse = await httpClient.GetAsync("forums");
+        var initialForums = await getInitialForumsResponse.Content.ReadFromJsonAsync<Forum[]>();
+        initialForums
+            .Should().NotBeNull().And
+            .Subject.As<Forum[]>().Should().BeEmpty();
+
+        using var response = await httpClient.PostAsync("forums", 
+            JsonContent.Create(new {title = "Test"}));
 
         response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
 
-        var result = await response.Content.ReadAsStringAsync();
+        var forum = await response.Content.ReadFromJsonAsync<Forum>();
 
-        result.Should().Be("[]");
+        forum
+            .Should().NotBeNull().And
+            .Subject.As<Forum>().Title.Should().Be("Test");
+
+        using var getForumsResponse = await httpClient.GetAsync("forums");
+        var forums = await getForumsResponse.Content.ReadFromJsonAsync<Forum[]>();
+        forums
+            .Should().NotBeNull().And
+            .Subject.As<Forum[]>().Should().Contain(f=>f.Title == "Test");
     }
+
+    //[Fact]
+    //public 
+    #region Old
+    //[Fact]
+    //public async Task ReturnListOfForums()
+    //{
+    //    using var httpClient = factory.CreateClient();
+    //    using var response = await httpClient.GetAsync("forums");
+
+    //    response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+
+    //    var result = await response.Content.ReadAsStringAsync();
+
+    //    result.Should().Be("[]");
+    //}
+    #endregion
 }
