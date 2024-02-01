433ea1c88495515b5bebf44cad27e43113da95d1
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-02-01 22:11:45 +0200

    add Topic Test

---
+++ TFA.E2E/TopicEndpointsShould.cs
@@ -0,0 +1,36 @@
+
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http.Json;
+using System.Reflection;
+using TFA.API.Models;
+using TFA.Storage;
+
+namespace TFA.E2E;
+
+public class TopicEndpointsShould(ForumApiApplicationFactory factory) 
+    : IClassFixture<ForumApiApplicationFactory>, IAsyncLifetime
+{
+    private readonly Guid forumId = Guid.Parse("027efe19-9659-418a-a872-56c4df2d8da8");
+    public Task DisposeAsync() => Task.CompletedTask;
+
+    public Task InitializeAsync() => Task.CompletedTask;
+
+    [Fact]
+    public async Task ReturnForbidden_WhenNotAuthenticated()
+    {
+        using var httpClient = factory.CreateClient();
+
+        using var forumCreateResponse = await httpClient.PostAsync("forums", JsonContent.Create(new { title = "Test forum" }));
+        forumCreateResponse.EnsureSuccessStatusCode();
+
+        var createdForum = await forumCreateResponse.Content.ReadFromJsonAsync<Forum>();
+        createdForum.Should().NotBeNull();
+
+
+        var response = await httpClient.PostAsync($"forums/{createdForum!.Id}/topics", 
+            JsonContent.Create(new { title = "Hello world" }));
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.InternalServerError);
+    }
+}
