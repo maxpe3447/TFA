675a8dca642c2ec5ab9f1146f5b20cef61f80bb0
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-02-01 21:14:34 +0200

    replace logging service registration

---
+++ TFA.API/DependencyInjection/LoggingServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Filters;
+
+namespace TFA.API.DependencyInjection;
+
+public static class LoggingServiceCollectionExtensions
+{
+    public static IServiceCollection AddApiLogging(this IServiceCollection services,
+        IConfiguration configuration,
+        IWebHostEnvironment webHostEnvironment)
+    {
+        return services.AddLogging(b => b.AddSerilog(new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .Enrich.WithProperty("Application", "TFA.API")
+    .Enrich.WithProperty("Environment", webHostEnvironment.EnvironmentName)
+    .WriteTo.Logger(lc => lc
+        .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+        .WriteTo.OpenSearch(
+        configuration.GetConnectionString("Logs"),
+        "forum-logs-{0:yyyy.MM.dd}"))
+    .WriteTo.Logger(lc => lc
+        .Filter.ByExcluding(Matching.FromSource("Microsoft"))
+        .WriteTo.Console())
+    .CreateLogger()));
+    }
+}
--- TFA.API/Program.cs
+++ TFA.API/Program.cs
@@ -2,32 +2,22 @@ using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 using Serilog.Filters;
-using TFA.API.Mapping;
+using System.Reflection;
+using TFA.API.DependencyInjection;
 using TFA.API.Middlewares;
 using TFA.Domain.DependencyInjection;
 using TFA.Storage.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddLogging(b => b.AddSerilog(new LoggerConfiguration()
-    .MinimumLevel.Debug()
-    .Enrich.WithProperty("Application", "TFA.API")
-    .Enrich.WithProperty("Environment", builder.Environment.EnvironmentName)
-    .WriteTo.Logger(lc => lc
-        .Filter.ByExcluding(Matching.FromSource("Microsoft"))
-        .WriteTo.OpenSearch(
-        builder.Configuration.GetConnectionString("Logs"),
-        "forum-logs-{0:yyyy.MM.dd}"))
-    .WriteTo.Logger(lc => lc
-        .Filter.ByExcluding(Matching.FromSource("Microsoft"))
-        .WriteTo.Console())
-    .CreateLogger()));
+builder.Services.AddApiLogging(builder.Configuration, builder.Environment);
 
 builder.Services
     .AddForumDomain()
-    .AddForumStorage(builder.Configuration.GetConnectionString("Postgres")??"");
+    .AddForumStorage(builder.Configuration.GetConnectionString("Postgres") ?? "");
 
 builder.Services
-    .AddAutoMapper(config => config.AddProfile<ApiProfile>());
+    .AddAutoMapper(config => config.AddMaps(Assembly.GetExecutingAssembly()));
+//.AddAutoMapper(config => config.AddProfile<ApiProfile>());
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
