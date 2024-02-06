e2ea13bb42f20a334d1b81d363f38bd9eb5ca201
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-02-07 01:07:57 +0200

    add CreateForumCommandValidator tests

---
+++ TFA.Domain.Tests/CreateForums/CreateForumCommandValidatorShould.cs
@@ -0,0 +1,31 @@
+using FluentAssertions;
+using TFA.Domain.UseCases.CreateForum;
+
+namespace TFA.Domain.Tests.CreateForums;
+
+public class CreateForumCommandValidatorShould
+{
+    private readonly CreateForumCommandValidator sut = new();
+
+    [Fact]
+    public void ReturnSuccess_WhenCommandValid()
+    {
+        var validCommand = new CreateForumCommand("Title");
+        sut.Validate(validCommand).IsValid.Should().BeTrue();
+    }
+
+    public static IEnumerable<object[]> GetInvalidCommand()
+    {
+        var validCommand = new CreateForumCommand("Title");
+
+        yield return new object[] { validCommand with { Title = string.Empty } };
+        yield return new object[] { validCommand with { Title = new string('a', 60) } };
+    }
+
+    [Theory]
+    [MemberData(nameof(GetInvalidCommand))]
+    public void ReturnFailure_WhenCommandInvalid(CreateForumCommand command)
+    {
+        sut.Validate(command).IsValid.Should().BeFalse();
+    }
+}
