202f5f6f586658b5b0309396b3aad140380b9ca0
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-02-07 00:43:57 +0200

    add CreateTopicResolveTests

---
+++ TFA.Domain.Tests/CreateTopic/CreateTopicResolverShould.cs
@@ -0,0 +1,34 @@
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore.InMemory.Query.Internal;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using Moq;
+using TFA.Domain.Authentication;
+using TFA.Domain.UseCases.CreateTopic;
+
+namespace TFA.Domain.Tests.CreateTopic;
+
+public class CreateTopicResolverShould
+{
+    private readonly TopicIntentionalResolver sut = new();
+    [Fact]
+    public void ReturnFalse_WhenIntentionNotInEnum()
+    {
+        var intention = (TopicIntention)(-1);
+        sut.IsAllowed(new Mock<IIdentity>().Object, intention)
+            .Should().BeFalse();
+    }
+
+    [Fact]
+    public void ReturnFalse_WhenCheckingTopicCreateIntention_AndUserIsGuest()
+    {
+        sut.IsAllowed(User.Guest, TopicIntention.Create).Should().BeFalse();
+    }
+    [Fact]
+    public void ReturnTrue_WhenCheckingTopicCreateIntention_AndUserIsAuthenticated()
+    {
+        sut.IsAllowed(
+            new User(Guid.Parse("20811a77-5b86-4e99-a0d1-6a4819b78721")),
+            TopicIntention.Create)
+            .Should().BeTrue();
+    }
+}
