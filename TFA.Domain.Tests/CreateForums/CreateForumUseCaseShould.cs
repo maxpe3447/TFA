482b71dd0f3d3af5927e64d7c0785d691fab21f4
maxpe3447 <36305245+maxpe3447@users.noreply.github.com>
2024-02-07 02:02:51 +0200

    add CreateForum test

---
+++ TFA.Domain.Tests/CreateForums/CreateForumUseCaseShould.cs
@@ -0,0 +1,57 @@
+using FluentAssertions;
+using FluentValidation;
+using FluentValidation.Results;
+using Moq;
+using Moq.Language.Flow;
+using TFA.Domain.Authorization;
+using TFA.Domain.Models;
+using TFA.Domain.UseCases.CreateForum;
+
+namespace TFA.Domain.Tests.CreateForums;
+
+public class CreateForumUseCaseShould
+{
+    private readonly Mock<ICreateForumStorage> storage;
+    private readonly ISetup<ICreateForumStorage, Task<Forum>> createForumSetup;
+    private readonly CreateForumUseCase sut;
+
+    public CreateForumUseCaseShould()
+    {
+        var validator = new Mock<IValidator<CreateForumCommand>>();
+        validator
+            .Setup(x => x.ValidateAsync(It.IsAny<CreateForumCommand>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new ValidationResult());
+
+        var intentionManager = new Mock<IIntentionManager>();
+        intentionManager
+            .Setup(x => x.IsAllowed(It.IsAny<ForumIntention>()))
+            .Returns(true);
+
+        storage = new Mock<ICreateForumStorage>();
+        createForumSetup = storage.Setup(s => s.Create(It.IsAny<string>(), It.IsAny<CancellationToken>()));
+
+        sut = new CreateForumUseCase(
+            validator.Object, intentionManager.Object, storage.Object);
+    }
+
+
+    [Fact]
+    public async Task ReturnCreatedForum()
+    {
+        var forum = new Forum
+        {
+            Id = Guid.Parse("ec5e548a-04ab-4b97-b223-325be04abf9d"),
+            Title = "Hello forum"
+        };
+
+        createForumSetup.ReturnsAsync(forum);
+
+        var actual = await sut.Execute(new CreateForumCommand("Hello"), CancellationToken.None);
+        actual.Should().Be(forum);
+
+        storage.Verify(s=>s.Create("Hello", It.IsAny<CancellationToken>()), Times.Once);
+        storage.VerifyNoOtherCalls();
+
+
+    }
+}
